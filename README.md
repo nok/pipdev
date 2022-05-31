a7d8b430d51db19c754fe534a66a1ed43e7b3b79
Darius Morawiec <nok@users.noreply.github.com>
2022-05-31 23:05:49 +0200

    add scaffold (main)

---
+++ README.md
@@ -0,0 +1,5 @@
+# pipdev
+
+## License
+
+The package is Open Source Software released under the [BSD 3-Clause License](LICENSE).
---
+++ pipdev/__init__.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def main():
+    print(sys.version)
+
+
+if __name__ == '__main__':
+    main()
+
+
+__version__ = '0.1.0'
