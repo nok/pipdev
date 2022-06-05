478dc7138bc29b18ec5c8487109062241d1c9fae
Darius Morawiec <nok@users.noreply.github.com>
2022-06-05 15:46:51 +0200

    add html output (main)

--- pipdev/__init__.py
+++ pipdev/__init__.py
@@ -8,14 +8,21 @@ from packaging.version import Version
 from tabulate import tabulate
 from colorama import init, Fore
 
-init()
-
 
 @dataclasses.dataclass
 class VersionCheck:
     version: Version
     specifier_set: SpecifierSet
     is_valid: bool
+
+    def print(self, fmt: str) -> str:
+        if fmt == 'html':
+            return f'<span style="color: {"green" if self.is_valid else "red"}">' \
+                   f'{self.version.public}' \
+                   f'</span>'
+        return f'{Fore.GREEN if self.is_valid else Fore.RED}' \
+               f'{self.version.public}' \
+               f'{Fore.RESET}'
 
 
 def _get_num_neighbours(num: int):
@@ -25,7 +32,8 @@ def _get_num_neighbours(num: int):
     yield num + 1
 
 
-def _get_phases_of_version(version: str, suffix_nums: List[int]) -> Iterator[Version]:
+def _get_phases_of_version(version: str,
+                           suffix_nums: List[int]) -> Iterator[Version]:
     yield Version(f'{version}')
     for n in suffix_nums:
         yield Version(f'{version}dev{n}')
@@ -34,7 +42,11 @@ def _get_phases_of_version(version: str, suffix_nums: List[int]) -> Iterator[Ver
         yield Version(f'{version}post{n}')
 
 
-def _generate_versions_for_version(majors: List[int], minors: List[int], micros: List[int], suffix_num: Optional[int] = None) -> Iterator[Version]:
+def _generate_versions_for_version(
+        majors: List[int],
+        minors: List[int],
+        micros: List[int],
+        suffix_num: Optional[int] = None) -> Iterator[Version]:
     if suffix_num:
         suffix_nums = [suffix_num - 1, suffix_num, suffix_num + 1]
         suffix_nums = list(filter(lambda x: x > 0, suffix_nums))
@@ -43,16 +55,18 @@ def _generate_versions_for_version(majors: List[int], minors: List[int], micros:
 
     for major in majors:
         if major > 0:
-            for version in _get_phases_of_version(str(major), suffix_nums=suffix_nums):
-                yield version
+            for ver in _get_phases_of_version(str(major), suffix_nums):
+                yield ver
         for minor in minors:
             if sum([major, minor]) > 0:
-                for version in _get_phases_of_version(f'{major}.{minor}', suffix_nums=suffix_nums):
-                    yield version
+                base_ver = f'{major}.{minor}'
+                for ver in _get_phases_of_version(base_ver, suffix_nums):
+                    yield ver
             for micro in micros:
                 if sum([major, minor, micro]) > 0:
-                    for version in _get_phases_of_version(f'{major}.{minor}.{micro}', suffix_nums=suffix_nums):
-                        yield version
+                    base_ver = f'{major}.{minor}.{micro}'
+                    for ver in _get_phases_of_version(base_ver, suffix_nums):
+                        yield ver
 
 
 def _generate_versions_for_specifier_set(specifier_set: str) -> List[Version]:
@@ -77,55 +91,70 @@ def _generate_versions_for_specifier_set(specifier_set: str) -> List[Version]:
             minors = list(_get_num_neighbours(minor))
             micros = list(_get_num_neighbours(micro))
 
-            generated_versions += list(_generate_versions_for_version(majors, minors, micros, suffix_num))
+            generated_versions += list(
+                _generate_versions_for_version(majors, minors, micros,
+                                               suffix_num))
 
     return generated_versions
 
 
-def _check_generated_versions_for_specifier_set(specifier_set: str) -> Iterator[VersionCheck]:
+def _check_generated_versions_for_specifier_set(
+        specifier_set: str) -> Iterator[VersionCheck]:
     versions = _generate_versions_for_specifier_set(specifier_set)
     specifier_set = SpecifierSet(specifiers=specifier_set)
     for version in versions:
         is_valid = version.public in specifier_set
-        yield VersionCheck(version=version, specifier_set=specifier_set, is_valid=is_valid)
+        yield VersionCheck(version=version,
+                           specifier_set=specifier_set,
+                           is_valid=is_valid)
 
 
-def print_similar_versions(specifier_set: str) -> str:
-    checks = _check_generated_versions_for_specifier_set(specifier_set)
+def print_similar_versions(specifier_set: str, fmt: str = 'github') -> str:
+    init()
+
+    version_checks = _check_generated_versions_for_specifier_set(specifier_set)
     base_versions = {}
     index = set()
-    for check in checks:
-        if check.version.public in index:
+    for version_check in version_checks:
+        if version_check.version.public in index:
             continue
         else:
-            index.add(check.version.public)
+            index.add(version_check.version.public)
 
-        base_version = check.version.base_version
+        base_version = version_check.version.base_version
         if base_version not in base_versions.keys():
-            base_versions[base_version] = dict(dev=[], pre=[], final=[], post=[])
-        if check.version.is_devrelease:
-            base_versions[base_version]['dev'].append(check)
-        elif check.version.is_prerelease:
-            base_versions[base_version]['pre'].append(check)
-        elif check.version.is_postrelease:
-            base_versions[base_version]['post'].append(check)
+            base_versions[base_version] = dict(dev=[],
+                                               pre=[],
+                                               final=[],
+                                               post=[])
+        if version_check.version.is_devrelease:
+            base_versions[base_version]['dev'].append(version_check)
+        elif version_check.version.is_prerelease:
+            base_versions[base_version]['pre'].append(version_check)
+        elif version_check.version.is_postrelease:
+            base_versions[base_version]['post'].append(version_check)
         else:
-            base_versions[base_version]['final'].append(check)
+            base_versions[base_version]['final'].append(version_check)
 
     base_versions = dict(sorted(base_versions.items()))
 
-    color_it = lambda check: f'{Fore.GREEN if check.is_valid else Fore.RED}{check.version.public}{Fore.RESET}'
-
     rows = []
-    for phase in base_versions.values():
+    for base_version in base_versions.values():
+        # Sort versions:
+        for phase, versions in base_version.items():
+            base_version[phase] = sorted(versions,
+                                         key=lambda x: x.version.public)
+        # Colorize versions:
         rows.append([
-            ' - '.join(sorted([color_it(check) for check in phase['dev']])),
-            ' - '.join(sorted([color_it(check) for check in phase['pre']])),
-            ' - '.join(sorted([color_it(check) for check in phase['final']])),
-            ' - '.join(sorted([color_it(check) for check in phase['post']]))
+            ' - '.join([
+                version_check.print(fmt=fmt) for version_check in base_version
+            ]) for base_version in base_version.values()
         ])
 
-    output = tabulate(rows, headers=('dev', 'pre', 'final', 'post'), tablefmt='github')
+    tablefmt = 'unsafehtml' if fmt == 'html' else fmt
+    output = tabulate(rows,
+                      headers=('dev', 'pre', 'final', 'post'),
+                      tablefmt=tablefmt)
     print(output)
     return output
 
@@ -136,11 +165,10 @@ def check(version: str, specifier_set: str):
 
 
 def main():
-    print_similar_versions('~=0.2b1,>0.3')
+    print_similar_versions('>=0.1b2,<=0.2')
 
 
 if __name__ == '__main__':
     main()
-
 
 __version__ = '0.1.0'
